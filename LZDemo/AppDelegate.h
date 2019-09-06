55146312a55895774a036e70b22e5f3fc0b69013
admin <admin@admins-Mac-mini.local>
2019-09-06 15:24:50 +0800

    init project

---
+++ LZDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZDemo
+//
+//  Created by admin on 2019/9/6.
+//  Copyright © 2019 rsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LZDemo
+//
+//  Created by admin on 2019/9/6.
+//  Copyright © 2019 rsh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
